c356bc7cfdc2cce1b97ef35bb2b1c03af172d14e
alxiom <hyoungseok.k@gmail.com>
2022-01-15 22:54:19 +0900

    [Python] add printing text

--- main.py
+++ main.py
@@ -7,6 +7,28 @@ running_time = 3 * 60 * 60  # second
 now = time.time()
 end_time = now + running_time
 
+
+def parse_text(file_name, slicing=48):
+    text = []
+    with open(file_name, "r", encoding="utf-8") as text_file:
+        read_line = text_file.readlines()
+        for line in read_line:
+            line = line.strip()
+            if len(line) > 0:
+                line_slice = [line[i:i + slicing] for i in range(0, len(line), slicing)]
+                text.extend(line_slice)
+    return text
+
+
+text_wave = parse_text("text_wave.txt")
+text_click = parse_text("text_click.txt")
+
+text_wave_length = len(text_wave)
+text_click_length = len(text_click)
+
+text_wave_cnt = 0
+text_click_cnt = 0
+
 sleep_second = 3.0
 id_vendor = 0x1fc9
 id_product = 0x2016
@@ -22,12 +44,17 @@ for i, device in enumerate(devices):
 print("Start Printing")
 while end_time - now > 0:
     for i, printer in enumerate(printers):
+        if i == 0:
+            message = text_wave[text_wave_cnt % text_wave_length]
+        elif i == 1:
+            message = text_click[text_click_cnt % text_click_length]
+        else:
+            date_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+            message = f"#{i + 1} / {date_time}"
         printer.reset()
-        date_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         lpr = os.popen("lpr", "w")
-        message = f"#{i + 1} / {date_time}"
         lpr.write(message)
         lpr.close()
-        print(message)
+        print(i, message)
         time.sleep(sleep_second)
     now = time.time()
