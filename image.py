815e676cb17872fb123698b6e114b90471c30dc4
안미빈 <bini@anmibin-ui-MacBookAir.local>
2022-02-03 14:53:13 +0900

    [Python] replace images

--- image.py
+++ image.py
@@ -6,15 +6,15 @@ running_time = 3 * 60 * 60  # second
 now = time.time()
 end_time = now + running_time
 
-image_length = 3
+image_length = 20
 image_cnt = 0
 sleep_second = 12.0
 
 print("Start Printing")
 while end_time - now > 0:
-    os.system(f"lpr -P {printer_name} pic{image_cnt % image_length:03d}.jpg")
+    os.system(f"lpr -P {printer_name} pic/{image_cnt % image_length:03d}.jpeg")
     image_cnt += 1
-    print(f"image_cnt={image_cnt} print pic{image_cnt % image_length:03d}.jpg")
+    print(f"image_cnt={image_cnt} print pic/{image_cnt % image_length:03d}.jpeg")
     time.sleep(sleep_second)
     now = time.time()
 
