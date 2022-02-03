95de78c50bfa4ab4969a18220867bc3bd1ff8557
안미빈 <bini@anmibin-ui-MacBookAir.local>
2022-02-03 12:47:43 +0900

    [Python] add image print code

---
+++ image.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+printer_name = "POS__Receipt_Printer"
+running_time = 3 * 60 * 60  # second
+now = time.time()
+end_time = now + running_time
+
+image_length = 3
+image_cnt = 0
+sleep_second = 12.0
+
+print("Start Printing")
+while end_time - now > 0:
+    os.system(f"lpr -P {printer_name} pic{image_cnt % image_length:03d}.jpg")
+    image_cnt += 1
+    print(f"image_cnt={image_cnt} print pic{image_cnt % image_length:03d}.jpg")
+    time.sleep(sleep_second)
+    now = time.time()
+
